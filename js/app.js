194c2e29f11fae7e42e3d34db310590623e86750
Xavier Lozinguez <xavier@xlprojects.com>
2013-06-24 15:06:21 -0400

    complete step 6

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@
     }
   });
 
+  // App.Album = Ember.Object.extend({
+  //   total_duration: function() {
+  //     var songs = this.get("songs");
+  //     var total = 0;
+  //     songs.forEach(function(song){
+  //       total += songs.get("duration");
+  //     });
+  //     return total;
+  //   }.property(this.songs)
+  // });
+
   App.AlbumRoute = Ember.Route.extend({
     model: function(params) {
       return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
@@ -17,6 +28,13 @@
 
   App.Router.map(function() {
     this.resource('album', { path: '/album/:album_id' });
+  });
+
+  Ember.Handlebars.helper('format-duration', function(value, options) {
+    var wholeMin = Math.floor(value/60);
+    var remainSec = value%60;
+    var wholeSec = (remainSec < 10) ? '0'+remainSec : remainSec;
+    return new Handlebars.SafeString(wholeMin+':'+wholeSec);
   });
 
 })();
