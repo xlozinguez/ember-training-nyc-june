e36d6f6324fe3d86abcffe31863bf0b1ec398b96
Xavier Lozinguez <xavier@xlprojects.com>
2013-06-24 11:23:13 -0400

    completed step 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 (function() {
-"use strict";
+  "use strict";
 
-window.App = Ember.Application.create();
+  window.App = Ember.Application.create();
+
+  App.ApplicationRoute = Ember.Route.extend({
+    model: function() {
+      return App.ALBUM_FIXTURES;
+    }
+  });
 
 })();
